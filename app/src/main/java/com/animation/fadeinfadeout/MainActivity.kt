ed7ebb6bc12f270092e5e5e45f3b52256978d7cb
Aysel Aydin <ayselaydin01@gmail.com>
2020-11-14 16:03:45 +0300

    Fade in Fade out animation completed

--- app/src/main/java/com/animation/fadeinfadeout/MainActivity.kt
+++ app/src/main/java/com/animation/fadeinfadeout/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.animation.fadeinfadeout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_fade_in.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            img_doit.startAnimation(animation)
+        }
+
+        btn_fade_out.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            img_doit.startAnimation(animation)
+        }
     }
 }
